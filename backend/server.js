62eb27af8a338e214d1be10b79bbdafe0b4ace4d
Om patel <patelom0901@gmail.com>
2026-02-25 00:33:12 +0530

    Successfully intergrated Authentication

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const app = express();
 
 //Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth",authRoutes);
 app.use("/api/products", productRoutes);
